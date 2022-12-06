2c15f3491acb45ba2537f06cd55cbf2b5853e378
Brian.Swedenberg Swedenberg <bswedenberg@gmail.com>
2022-12-06 00:06:47 +0000

    Added function to front page

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,15 @@ import AppAGGroup from './components/AppAGGroup'
 import logoType from './images/Logotype1.png'
 import AppAGServerSide from './components/AppAGServerSide'
 
+export function initNetlifyIdentity() {
+  console.log('netlify ID was called ...')
+  const script = document.createElement('script');
+  script.src = "https://identity.netlify.com/v1/netlify-identity-widget.js"
+  script.async = true;
+
+  document.body.appendChild(script)
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
     <img src={logoType}/>
