e84af8785f8824c68d85f340c7a3191ed1856953
Brian.Swedenberg Swedenberg <bswedenberg@gmail.com>
2022-12-07 22:14:17 +0000

    Console logs

--- src/index.jsx
+++ src/index.jsx
@@ -11,10 +11,19 @@ import netlifyIdentity from 'netlify-identity-widget'
 
 window.netfliyIdentity = netlifyIdentity;
 
-// // netlifyIdentity.open();
-netlifyIdentity.init();
-
+const user = netlifyIdentity.currentUser();
 netlifyIdentity.open();
+netlifyIdentity.on('init', user => console.log('init', user));
+netlifyIdentity.on('login', user => console.log('login', user));
+netlifyIdentity.on('logout', () => console.log('Logged out'));
+netlifyIdentity.on('error', err => console.error('Error', err));
+netlifyIdentity.on('open', () => console.log('Widget opened'));
+netlifyIdentity.on('close', () => console.log('Widget closed'));
+
+// // netlifyIdentity.open();
+// netlifyIdentity.init();
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
