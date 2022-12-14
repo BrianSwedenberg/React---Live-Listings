db99e5241df73718ef8b20857065db2970ae87ea
Brian.Swedenberg Swedenberg <bswedenberg@gmail.com>
2022-12-14 22:29:43 +0000

    updated imports

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { useState } from "react";
 import { useNavigate } from "react-router";
 import netlifyIdentity from 'netlify-identity-widget'
 import styled from 'styled-components';
