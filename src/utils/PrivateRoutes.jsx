1953ac05766b7e1dd679de1e9bb9f293ecf51054
Brian.Swedenberg Swedenberg <bswedenberg@gmail.com>
2022-12-14 16:05:26 +0000

    commenting out debugging on router

--- src/utils/PrivateRoutes.jsx
+++ src/utils/PrivateRoutes.jsx
@@ -22,9 +22,9 @@ let auth = false;
 if (user_roles == null ) { console.log('undefined user roles') }
 else if (user_roles.includes('TestUser')) { 
   {auth=true}; 
-  console.log('auth is true');
-  console.log('user_roles exists');
-  console.log(user_roles);
+  // console.log('auth is true');
+  // console.log('user_roles exists');
+  // console.log(user_roles);
 }
 
 
