9dd8028240167b6713f315daf1d89f388c828471
Brian.Swedenberg Swedenberg <bswedenberg@gmail.com>
2022-12-14 23:26:08 +0000

    navigate(-1)

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -30,7 +30,8 @@ const Login = () => {
   
   const handleLogin = (params) => {
     console.log('handle login - ', params);  
-    navigate("/");
+    // navigate("/", { replace: true});
+    navigate(-1);
   }
 
   netlifyIdentity.on('login', user => handleLogin(user))
