a90a8005a3d39c8a341b07717b67f0b8e3cb47c9
Brian.Swedenberg Swedenberg <bswedenberg@gmail.com>
2022-12-15 00:00:51 +0000

    refactored privateroute

--- src/utils/PrivateRoutes.jsx
+++ src/utils/PrivateRoutes.jsx
@@ -1,35 +1,36 @@
 import { Outlet, Navigate } from 'react-router-dom';
 import netlifyIdentity from 'netlify-identity-widget';
 
-window.netfliyIdentity = netlifyIdentity;
-netlifyIdentity.init();
-const user = netlifyIdentity.currentUser();
-var user_metadata = null; 
-var user_roles = null;
-
-if (user == null ) { console.log('null user'); }
-else{  
-  console.log('user not null');
-  user_metadata = user['app_metadata']; 
-  user_roles = user_metadata['roles']; 
-  console.log(user)
-  // console.log(user_metadata)
-  // console.log(user_roles)
-  // console.log(typeof user_roles)
-}
-
-let auth = false;
-if (user_roles == null ) { console.log('undefined user roles') }
-else if (user_roles.includes('TestUser')) { 
-  {auth=true}; 
-  console.log('auth is true');
-  // console.log('user_roles exists');
-  // console.log(user_roles);
-}
-
 const PrivateRoutes = () => {
-  console.log('auth - ', auth);
-  console.log('user info - ', user);
+
+  window.netfliyIdentity = netlifyIdentity;
+  netlifyIdentity.init();
+  const user = netlifyIdentity.currentUser();
+  var user_metadata = null; 
+  var user_roles = null;
+  
+  if (user == null ) { console.log('null user'); }
+  else{  
+    console.log('user not null');
+    user_metadata = user['app_metadata']; 
+    user_roles = user_metadata['roles']; 
+    console.log(user)
+    // console.log(user_metadata)
+    // console.log(user_roles)
+    // console.log(typeof user_roles)
+  }
+  
+  let auth = false;
+  if (user_roles == null ) { console.log('undefined user roles') }
+  else if (user_roles.includes('TestUser')) { 
+    {auth=true}; 
+    console.log('auth is true');
+    // console.log('user_roles exists');
+    // console.log(user_roles);
+  }
+
+  // console.log('auth - ', auth);
+  // console.log('user info - ', user);
   return(
     auth ? <Outlet /> : <Navigate to='/login'/>
   );
